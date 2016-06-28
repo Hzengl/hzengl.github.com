b8d01c9ed39266d1f3d25dc51de12d218aa6ad5e
Hzengl <1455250540@qq.com>
2016-06-28 20:49:23 +0800

    WorldFoot

---
+++ WorldFoot/js/addons.js
@@ -0,0 +1,28 @@
+
+(function (doc, win) {
+	  var docEl = doc.documentElement,
+		resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+		recalc = function () {
+		  var clientWidth = docEl.clientWidth;
+		  if (!clientWidth) return;
+		  docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+		};
+
+	  if (!doc.addEventListener) return;
+	  win.addEventListener(resizeEvt, recalc, false);
+	  doc.addEventListener('DOMContentLoaded', recalc, false);
+	})(document, window);
+	
+
+$(function(){
+	
+	
+	//页面不足一屏，铺满一屏
+	$('.boxHeight').css('min-height',$(window).height());
+
+	
+	
+
+
+})
+
